fdfadd4682248075ad672765b77cdbb9269c3ab2
Alexia <itangishakaalexia0@gmail.com>
2025-03-18 11:02:07 +0200

    arduino

--- 24RP15724ligharduino.ino
+++ 24RP15724ligharduino.ino
@@ -1,26 +1,22 @@
 // Traffic Light Simulation
-int bluePin = 13;
+int redPin = 13;
 int yellowPin = 12;
 int greenPin = 11;
-
-const int blueDuration = 1000; 
+const int redDuration = 1000; 
 const int yellowDuration = 1000; 
 const int greenDuration = 1000; 
 void setup() {
-  pinMode(bluePin, OUTPUT);
+  pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
 }
 void loop() {
-  
-  digitalWrite(bluePin, HIGH);
-  delay(blueDuration);
-  digitalWrite(bluePin, LOW);
-  
+  digitalWrite(redPin, HIGH);
+  delay(redDuration);
+  digitalWrite(redPin, LOW);
   digitalWrite(yellowPin, HIGH);
   delay(yellowDuration);
   digitalWrite(yellowPin, LOW);
-  
   digitalWrite(greenPin, HIGH);
   delay(greenDuration);
   digitalWrite(greenPin, LOW);
